2437581060c962cbd66d02b261c0a8c75d8f848e
MorningTZH <morningtzh@yeah.net>
2018-05-29 19:27:32 +0800

    signature

--- server/api/mp.py
+++ server/api/mp.py
@@ -1,6 +1,7 @@
 from flask import session, request
 import flask_restful
 from funcs.base.config import CONFIG
+import hashlib
 
 class Mp(flask_restful.Resource):
 
@@ -9,5 +10,15 @@ class Mp(flask_restful.Resource):
         
         print(request.args)
         print(request.args.keys())
+
+        signature = request.args.get("signature")
+        timestamp = request.args.get("timestamp")
+        nonce = request.args.get("nonce")
+        token = "lalala"
+
+        check_sign = hashlib.sha1("".join([token,timestamp,nonce].sort()))
+
+        print(signature, check_sign)
+
 
         return {"errno":200}
